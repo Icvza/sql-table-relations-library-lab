46efb5203abc0c783dd88dd777967b76345d8340
Isaac <devisaacalejandro@gmail.com>
2021-01-23 15:52:26 -0500

    commit tree 8999d5fdf226220bbb4679b80c8c97164b9203cd

--- lib/querying.rb
+++ lib/querying.rb
@@ -29,6 +29,6 @@ def select_character_names_and_number_of_books_they_are_in
   "SELECT name, COUNT(character_books.character_id) FROM characters 
   JOIN character_books ON characters.id = character_books.character_id 
   GROUP BY name
-  ORDER BY 2 DESC LIMIT 1
+  ORDER BY 2 DESC, 1
   ;"
 end
